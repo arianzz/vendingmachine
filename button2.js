e017193d69da4c60812af9538fe48dcb960238f6
arianzz <av1834@nyu.edu>
2017-03-07 18:04:42 -0500

    Add files via upload

---
+++ button2.js
@@ -0,0 +1,22 @@
+var GPIO = require('onoff').Gpio,
+    led = new GPIO(18, 'out'),
+    button = new GPIO(16, 'in', 'both');
+
+// define the callback function
+function light(err, state) {
+
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
+    led.writeSync(1);
+  } else {
+    // turn LED off
+    led.writeSync(0);
+  }
+
+}
+
+// pass the callback function to the
+// as the first argument to watch()
+button.watch(light);
