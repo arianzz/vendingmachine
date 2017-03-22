fe0b3e3388a9cbf9a85366500ffa267b66813ef8
Ariana Vassilopoulou <arianavassilopoulou@172-16-245-188.DYNAPOOL.NYU.EDU>
2017-03-22 14:09:30 -0400

    new images

--- button2.js
+++ button2.js
@@ -6,8 +6,9 @@ http.createServer(function(req, res) {
     // sending a response header of 200 OK
     res.writeHead(200, {
         'Content-Type': 'text/plain'
+    });
     // print out Hello World
-    res.end('Hello World\n');
+    res.send('Hello World\n');
     // use port 8080
 
 }).listen(8080);
