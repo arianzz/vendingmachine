c921d8de85db321a5427ca84f73d375cb6ba947f
Ariana Vassilopoulou <arianavassilopoulou@172-16-240-208.DYNAPOOL.NYU.EDU>
2017-03-24 19:36:02 -0400

    new messages

---
+++ frontendjavascript.js
@@ -0,0 +1,9 @@
+$(function photos() {
+    var socket = io();
+    $('art').submit(function(data) {
+        socket.emit('art', 'vendingmachine/1.png');
+        return false;
+    });
+    socket.on('art', function(photos);
+    });
+});
