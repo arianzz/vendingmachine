2cc0879c545414247ce09aa63e3df460d001e2f7
Ariana Vassilopoulou <arianavassilopoulou@172-16-240-41.DYNAPOOL.NYU.EDU>
2017-03-31 20:00:20 -0400

    added public foler

---
+++ public/frontendjavascript.js
@@ -0,0 +1,14 @@
+console.log('frontendjavascript.js is running');
+
+/*
+$(function photos() {
+    var socket = io();
+    $('art').submit(function(data) {
+        socket.emit('art', 'vendingmachine/1.png');
+        return false;
+    });
+    socket.on('art', function(photos){
+	console.log(photos);
+	});
+});
+*/
